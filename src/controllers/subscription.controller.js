5ae45576f4d62dce611378db322879bbf9c3dfdb
Yash Singh Verma <yashsvlight@gmail.com>
2024-09-05 20:49:36 +0530

    One subscription controller made

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,6 +9,48 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    
+
+    if(!channelId){
+        throw new ApiError(400,"Channel Id is required")
+    }
+
+    const user = req.user
+
+    if(channelId===user?._id?.toString()){
+        throw new ApiError(400, "User cannot subscribe or remove subscription for himself")
+    }
+
+    const Subscribed = await Subscription.find({subscriber: user, channel: channelId})
+
+    if(Subscribed.length==0){
+
+        // console.log(Subscribed);
+        
+        const Subscriber = await Subscription.create({
+            subscriber: user?._id,
+            channel: channelId
+        })
+
+        if(!Subscriber){
+            throw new ApiError(501, "Internal Server Error")
+        }
+
+        return res
+                .status(200)
+                .json(new ApiResponse(200, Subscriber, "Subscription Added"))
+    }
+
+    const deleted = await Subscription.deleteOne({subscriber: user, channel: channelId})
+
+    if(!deleted){
+        throw new ApiError(500, "Error in deleting subscription")
+    }
+
+    return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "Subscription Removed Successfully"))
+
 })
 
 // controller to return subscriber list of a channel
